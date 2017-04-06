da1705a9339ac315b92fa5a098cae9c59dfee474
Quentin Fumoux <fumoux.quentin@gmail.com>
2017-04-06 10:10:23 +0200

    Modif propriétaire

--- proprietaires.php
+++ proprietaires.php
@@ -9,11 +9,19 @@
     <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-    <?php include 'navbar.php'; ?>
+    <?php include 'navbar.php';include 'configuration.php'; ?>
     <div class="container"> <!-- Contient toute la page -->
         <?php
             echo '<h1>Liste des propriétaires</h1><br/>';
-            
+            echo '<table>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                        <th>Titre</th>
+                        <th>Mobile</th>
+                        <th>Téléphone perso.</th>
+                        </tr>';
+
         ?>
     </div>
 </body>
